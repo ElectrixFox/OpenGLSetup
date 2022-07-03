7665751128f4a34897fe1daec672d93e60de996b
ElectricCow <lukecphillips2006@gmail.com>
2022-07-03 20:22:03 +0100

    A few clean ups
    Just tidied up the main.cpp file so that it is a bit smaller.

--- main.cpp
+++ main.cpp
@@ -25,48 +25,6 @@ int main()
     GLFWwindow* window = CreateWindow(960, 540, "Hello World!");
     InitialiseGraphics();
     initMatricies();
-
-/* float vertex[] =
-    {
-        0.5f,  0.5f, 0.0f, 1.0f, 1.0f,
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-       -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
-       -0.5f,  0.5f, 0.0f, 0.0f, 1.0f
-    };
-
-    unsigned int index[] =
-    {
-        0, 1, 3,
-        1, 2, 3
-    }; 
-
-    unsigned int shader = CreateShader("res/shader.shader");
-    SetUniformM4(shader, "U_Transform", M4_Identity());
-    SetUniform4f(shader, "U_Colour", 1.0, 0.0, 0.0, 1.0);
-
-    unsigned int vao = CreateVertexArray();
-    unsigned int vbo = CreateVertexBuffer(vertex, sizeof(vertex));
-    unsigned int ibo = CreateIndexBuffer(index, sizeof index);
-
-    unsigned int shader2 = CreateShader("res/texShader.shader");
-    unsigned short int texture = CreateTexture("res/Boris.png");
-    SetUniformM4(shader, "U_Transform", M4_Identity());
-    SetUniform4f(shader, "U_Colour", 1.0, 0.0, 0.0, 1.0);
-
-    SetUniform1i(shader, "U_Texture", 0);
-
-    unsigned int vao2 = CreateVertexArray();
-    unsigned int vbo2 = CreateVertexBufferDepth(vertex, sizeof(vertex), 0, 3, 5, 0);
-    AddAttribute(1, 2, 5, 3);
-
-    unsigned int shader1 = CreateShader("res/shader.shader");
-    SetUniformM4(shader1, "U_Transform", M4_Identity());
-    SetUniform4f(shader1, "U_Colour", 1.0, 0.5, 0.0, 1.0);
-
-    unsigned int vao1 = CreateVertexArray();
-    unsigned int vbo1 = CreateVertexBuffer(vertex, sizeof(vertex));
-    unsigned int ibo1 = CreateIndexBuffer(index, sizeof index); */
-    
     
     using namespace std;
 
@@ -81,7 +39,7 @@ int main()
     entities.push_back(entity2);
     entities.push_back(entity3);
 
-    RenderComponent rend_ent = CreateNewSquare(&world, entity, 0);
+    CreateNewSquare(&world, entity);
     CreateNewSquare(&world, entity2, "res/Boris.png");
     CreateNewSquare(&world, entity3);
 
